5c5205fa075ed42abf98614b8087b0f5b84ed6b5
jeongwoo <mbh219@naver.com>
2022-08-11 14:42:21 +0900

    backend2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-// localhost:8000/
+// localhost:8000 , 3000 8080 많이 사용함/
 app.get("/", (req,res)=>{
     res.sendFile(__dirname+"/hello.html");
 })
