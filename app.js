e300d3fd0a9ad98b029500ca31a616b5710bb8f4
jeongwoo <mbh219@naver.com>
2022-08-11 14:58:00 +0900

    study: create node js server

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 8000;
-// localhost:8000 , 3000 8080 많이 사용함/
-app.get("/", (req,res)=>{
-    res.sendFile(__dirname+"/hello.html");
+
+const port = 8000; 
+// 3000, 8080 -> 주로 많이 사용함
+// ip:8000/
+app.get("/",(req,res)=>{
+    res.send("hello");
 })
 app.listen(port, ()=>{
-    console.log("server open: ", port);
+    console.log("server open:", port)
 })
+
