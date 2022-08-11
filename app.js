2957cfec478d902a7148b76ae077701e18a9f962
jeongwoo <mbh219@naver.com>
2022-08-11 15:35:04 +0900

    study:node js app use

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.use("/static",express.static('static'));
+
 const port = 8000; 
 // 3000, 8080 -> 주로 많이 사용함
 // ip:8000/
+// server 끌때는 컨트롤 v
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/hello.html");
+    res.render("test");
 })
 app.listen(port, ()=>{
     console.log("server open:", port)
