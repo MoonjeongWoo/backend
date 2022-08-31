228519f9412bdce6fe11e3136fd2a8b68547eb9f
jeongwoo <mbh219@naver.com>
2022-08-31 14:04:11 +0900

    backend:commit용도

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require("express");
+const multer = require("multer");
 const app = express();
-app.set("view engine", "ejs")
-app.use("/static", express.static("static"))
-const port = 8000;
-// localhost:8000/
-app.get("/",(req,res)=>{
-    res.render("test");
-})
-app.get("/test02",(req,res)=>{
-    res.render("test02");
-})
-app.get("/test03",(req,res)=>{
-    res.render("test03");
-})
-app.get("/test04",(req,res)=>{
-    var person =[
-        {nmae: "문정우" , gender:"male"}
-    ]
-    res.render("test",{person: person});
-})
+// 파일 제출할때 파일이 저장되는 공간
+// dest : uploads
+const upload = multer({
+  dest: "uploads/",
+});
 
-app.listen(port, ()=>{
-    console.log("server open: ", port);
-})
+app.set("view engine", "ejs");
+app.use("/static", express.static("static"));
+
+const port = 8000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("main");
+});
+// 파일 post하기
+// post("/") <- 요놈은 form 태그 action하고 같은놈 ,
+// upload.single은 form태그 name하고 같은 놈,
+//
+app.post("/upload", upload.single("userfile"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.send("업로드성공했어요!");
+});
+
+app.listen(port, () => {
+  console.log("server open: ", port);
+});
