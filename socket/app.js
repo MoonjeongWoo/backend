8c2dbbcdae756ef34c39c36a9ea2e46bb6275bdc
jeongwoo <mbh219@naver.com>
2022-09-16 15:36:22 +0900

    2nd commit

--- socket/app.js
+++ socket/app.js
@@ -7,16 +7,28 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   res.render("index");
 });
+
+var client_list = {};
+
 io.on("connection", function (socket) {
-  io.emit("notice", socket.id);
+  // io.emit("notice", socket.id);
   socket.on("sendMsg", (msg) => {
     // msg 받아서 전체 클라이언트한테 전송
     io.emit("send", msg);
   });
-  socket.on("disconnect", function (id) {
-    io.emit("disconnected", socket.id);
+
+  socket.on("setNick", function (nick) {
+    if (Object.values(client_list).indexOf(nick) > -1) {
+      socket.emit("err", "이미 있는 이름이유");
+    } else {
+      client_list[socket.id] = nick;
+      // console.log(client_list);
+      io.emit("notice", nick);
+      socket.emit("entrySuccess", "입장성공");
+    }
+    // 딕셔너리에서 value값만 갖고 오기
+    //배열에서 원하는 값의 존재 여부 함수 => arr.indexOf()
   });
-  socket.emit("socket_id", socket.id);
 });
 
 http.listen(port, function () {
